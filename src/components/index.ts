abcc2cb1d4fcb8cd9368b9044cfc39ea8653401c
Dustin Schau <dustinschau@gmail.com>
2017-11-14 16:31:38 -0600

    remove gatsby and commit basic scaffolding

--- src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './Content';
-export * from './Footer';
-export * from './Header';
-export * from './SubHeader';
--- src/style/theme.ts
+++
@@ -1,7 +0,0 @@
-export const THEME = {
-  header: '#111',
-  base: '#2B3A42',
-  baseLight: '#3F5865',
-  secondary: '#F15623',
-  text: '#F0EFEF',
-};
