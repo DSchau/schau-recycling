82cf11c6307af97d3421730e5cc2671114d2f839
Dustin Schau <dustinschau@gmail.com>
2017-11-14 21:26:47 -0600

    feat: add basic scaffolding

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 import 'normalize.css';
 import './style.css';
+import autosize from 'autosize';
+import fetch from 'unfetch';
+
+autosize(document.querySelectorAll('textarea'));
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', ev => {
+  ev.preventDefault();
+  const elements = Array.from(form.querySelectorAll('input')).concat(form.querySelector('textarea'));
+
+  const valid = elements
+    .some(element => element.value.trim() === '') === false;
+
+  if (valid) {
+
+  }
+});
